2d173cf10537f3928010025a1aa0d439a9a1c6f7
QuangBoo <19522099@gm.uit.edu.vn>
2022-06-10 11:45:02 +0700

    code

---
+++ Đồ án TKCSDL/code FE/asset/index_signin.js
@@ -0,0 +1,65 @@
+// var user = document.querySelectorAll('#user__click');
+// var information = document.getElementById("user__detail");
+// var window = document.getElementsTagName("body");
+// user.onclick = function() {
+//     information.style.display = "block";
+//     return false;
+// }
+// window.onclick = function(event) {
+//         if (event.target == user) {
+//           information.style.display = "none";
+//      }
+// }
+// for (i = 0; i < user.length; i++)
+// {
+//     user.onclick = function() {
+//         information.style.display = "block";
+//     }
+//     window.onclick = function(event) {
+//         if (event.target == information) {
+//           information.style.display = "none";
+//         }
+//     }
+//  }
+
+        const buyBtns = document.querySelectorAll('.js_open')
+        const modal = document.querySelector('.js_contain')
+        const modalClose = document.querySelector('.container')
+        const modalClose2 = document.querySelector('.container-list')
+        const modalContainer = document.querySelector('.js-modal-container')
+
+        //hàm hiển thị modal(thêm class open vào modal)
+        function showBuyTickets(){
+            modal.classList.add('open')
+        }
+
+        //hàm ẩn modal(gỡ bỏ class open của modal)
+        function hideBuyTickets(){
+            modal.classList.remove('open')
+        }
+
+        //Lặp qua từng thẻ button và lắng nghe hành vi click
+        for(const buyBtn of buyBtns){
+            buyBtn.addEventListener('click', showBuyTickets)
+        }
+
+        //Nghe hành vi click vào button close
+        modalClose.addEventListener('click', hideBuyTickets)
+        modalClose2.addEventListener('click', hideBuyTickets)
+///////////////
+        var slideIndex = 0;
+        carousel();
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("container-banner__frame");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+          }
+          slideIndex++;
+          if (slideIndex > x.length) {slideIndex = 1}
+          x[slideIndex-1].style.display = "block";
+          setTimeout(carousel, 3000); // Change image every 3 seconds
+        }
+
+        
