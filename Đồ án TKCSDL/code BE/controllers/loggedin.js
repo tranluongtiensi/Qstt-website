40c601e1f3313d0f97084fb1c1d2d769cb074dbc
Trần Lương Tiến Sĩ - 19522128 <91487292+tranluongtiensi@users.noreply.github.com>
2022-06-18 00:12:31 +0700

    sdsd

---
+++ Đồ án TKCSDL/code BE/controllers/loggedin.js
@@ -0,0 +1,16 @@
+const User = require ('../model/user')
+const jwt = require ('jsonwebtoken')
+const loggedin = (req, res, next) => {
+    if (!req.cookies.userRegister ) return next()
+    try {
+        const decode = jwt.verify(req.cookies.userRegister, process.env.JWT_SECRET)
+        User.query ('SELECT * FROM users where id = ?', [decode.id], (error, result) => {
+            if (error) return next()
+            req.user = result[0]
+            return next()
+        })
+    } catch (error) {
+        if(err) return next()
+    }
+}
+module.exports = loggedin
--- Đồ án TKCSDL/code BE/server.js
+++ Đồ án TKCSDL/code BE/server.js
@@ -4,11 +4,16 @@ const User = require ('./model/user')
 const bodyParser = require('body-parser')
 const bcrypt = require ('bcryptjs')
 const jwt = require ('jsonwebtoken')
+const cookie = require ('cookie-parser')
+const loggedin = require ('./controllers/loggedin')
 const port =3000
 
 const app = express()
-app.use('/', express.static(path.join(__dirname, '../code FE/')))
+app.set('view engine', 'ejs')
+app.use(cookie());
+app.use('/',loggedin, express.static(path.join(__dirname, '../code FE/')))
 app.use(bodyParser.json())
+app.set('view engine', 'ejs')
 
 User.connect((err) => {
     if (err) throw err
@@ -20,19 +25,22 @@ User.connect((err) => {
 //   const user = jwt.verify(token, JWT_SECRET)
 //   console.log()
 // })
-// || !await bcrypt.compare(password, result[0].password
 
 app.post('/signin', (req, res) => {
-    const { username, password } = req.body
-    if (!password || !username) {
+    const { username, password: plainTextPassword} = req.body
+    //no value entered
+    if (!plainTextPassword || !username) {
       return res.json({status: 'error', error: 'Invalid username/ password'})
     }
     else {  
-      User.query('SELECT username, password FROM users WHERE username = ?', [username], async (error, result) => {
+      User.query('SELECT * FROM users WHERE username = ?', [username], async (error, result) => {
         if (error) throw error
-        if ( !result[0] || !await bcrypt.compare('password', result[0].password)) {
-
+        const validPassword = await bcrypt.compare(plainTextPassword, result[0].password)
+        console.log(validPassword)
+        //enter wrong username or password 
+        if ( !result[0] || !validPassword) {
           return res.json({status: 'error', error: 'Incorrect username or password'}) }
+        //In case the username, password is entered correctly
         else {
           const token = jwt.sign({
             id: result[0].id, 
@@ -40,13 +48,13 @@ app.post('/signin', (req, res) => {
           },
           process.env.JWT_SECRET,{
           expiresIn: process.env.JWT_EXPIRES
-          // httpOnly: true
           })
           const cookie_option = {
             expiresIn: new Date(Date.now() + process.env.COOKIE_EXPIRES *24 *60 *60 *1000),
-            httpOnly: true
+            httpOnly: true,
+            secure: false
           }
-          res.cookie('user Register', token, cookie_option)
+          res.cookie('userRegister', token, cookie_option)
           return res.json({status: 'ok', success: 'user has been logged in', data: token})
         }
       })
@@ -58,15 +66,33 @@ app.post('/register', async (req, res) => {
     if (!username || typeof username !== 'string') {
        return res.json({ status: 'error', error: 'Invalid username'})
     }
-    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+    if (!plainTextPassword || typeof someOtherPlaintextPassword !== 'string') {
        return res.json({ status: 'error', error: 'Invalid password'})
     }
-    User.query('SELECT username FROM users WHERE username = ?', [username], async (error, result) => {
+    User.query('SELECT * FROM users WHERE username = ?', [username], async (error, result) => {
+
         if (error) throw error
-        if (result[0]) return res.json({status: 'error', error: 'user already in use'})
+        if (result[0]) {
+          const validPassword = await bcrypt.compare(plainTextPassword, result[0].password) 
+          const validPasswordConfirmation =  await bcrypt.compare(someOtherPlaintextPassword, result[0].password_confirmation)
+          /* compare the password confirmation, password and phone number if it 
+          is the same as the password, password confirmation, phone number stored in mysql.
+          If it's the same then show message "user already in use please login"
+          */
+          if ( validPassword && validPasswordConfirmation && phone == result[0].phone) {
+            return res.json({status: 'ok', error: 'user already in use please login'})
+          }
+          if (phone != result[0].phone){
+
+            return res.json({status: 'error', error: 'username is already in use, please enter another username'})
+          }
+          else{
+            return res.json({status:'error', error: 'username is already in use, please enter another username'})
+          }
+        }
         else {
-            const password = await bcrypt.hash('password', 10)
-            const password_confirmation = await bcrypt.hash('password_confirmation', 10)
+            const password = await bcrypt.hash(plainTextPassword, 10)
+            const password_confirmation = await bcrypt.hash(someOtherPlaintextPassword, 10)
             post = {username: username, password: password, password_confirmation: password_confirmation, phone: phone}
             User.query('INSERT INTO users SET ?', post, (error, result) => {
                 if (error) throw error
